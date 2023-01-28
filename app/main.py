77ff55e9562c3ba839cc263e3eafcb6c7a9a3b4b
LimitBreaker98 <jorgeandresesguerraa@gmail.com>
2023-01-28 18:53:57 -0500

    sending default response to PING redis command

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
 import socket
 
+"""
+    A convenience class to handle strings the way RESP (REdis Serialization Protocol) expects it
+"""
+class RESPString:
+    def __init__(self, s: str) -> None:
+        self.s = s
+    def as_simple_string(self) -> str:
+        return f"+{self.s}\r\n"
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,9 +16,10 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client, address = server_socket.accept() # wait for client
-    ping_response = "PONG"
-    client.send(ping_response.encode())
+    client, _ = server_socket.accept() # wait for client
+    resp = RESPString("PONG")
+    client.send(resp.as_simple_string().encode())
+    server_socket.close()
     
     
 
