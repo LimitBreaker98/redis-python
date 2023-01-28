691c0102b42d23e253b1ee3d0e20463080cd42fc
LimitBreaker98 <jorgeandresesguerraa@gmail.com>
2023-01-28 18:21:03 -0500

    test

--- app/main.py
+++ app/main.py
@@ -8,8 +8,10 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    host_address, port = server_socket.accept() # wait for client
-    print(host_address, port)
+    client, address = server_socket.accept() # wait for client
+    ping_response = "PONG"
+    client.send(ping_response.encode())
+    
     
 
 if __name__ == "__main__":
