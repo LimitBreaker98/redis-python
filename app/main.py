b8a5f24d0eccff73aed67c257cd9d0aaf9a5d8ac
LimitBreaker98 <jorgeandresesguerraa@gmail.com>
2023-01-28 18:10:07 -0500

    testing return from socket.accept

--- app/main.py
+++ app/main.py
@@ -8,7 +8,9 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    host_address, port = server_socket.accept() # wait for client
+    print(host_address, port)
+    
 
 if __name__ == "__main__":
     main()
