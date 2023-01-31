e07bda292d05ca703a95abe1747a2ad6f62a0bca
LimitBreaker98 <jorgeandresesguerraa@gmail.com>
2023-01-31 12:15:02 -0500

    testing multiple responses to pings

--- app/main.py
+++ app/main.py
@@ -11,13 +11,18 @@ class RESPString:
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    while True:
-        client_socket, _ = server_socket.accept() # wait for client
-        resp = RESPString("PONG")
+    server_socket.settimeout(1.0)
+    timed_out = False
+    while not timed_out:
+        try:
+            client_socket, _ = server_socket.accept() # wait for client
+        except socket.timeout:
+            print("entered")
+            timed_out = True
+        else:
+            resp = RESPString("PONG")
         client_socket.send(resp.as_simple_string().encode())
-        
-    
-    
+
 
 if __name__ == "__main__":
     main()
