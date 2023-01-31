4212b7dba8009912e132704493b50ff6257a53af
LimitBreaker98 <jorgeandresesguerraa@gmail.com>
2023-01-31 12:37:20 -0500

    fixing PONG response

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ class RESPString:
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_connection, _ = server_socket.accept() # accepts a single client connection.
-    response_string = RESPString("Pong").as_simple_string()
+    response_string = RESPString("PONG").as_simple_string()
 
     while client_connection.recv(1024):  # While the server receives data from the client connection.
         client_connection.send(response_string.encode())
