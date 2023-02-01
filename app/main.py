9f3bcfa6591bb28294217597306c061eafa9b4a8
LimitBreaker98 <jorgeandresesguerraa@gmail.com>
2023-02-01 16:01:11 -0500

    fixing PONG response

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 """
     A convenience class to handle strings the way RESP (REdis Serialization Protocol) expects it
@@ -9,14 +10,23 @@ class RESPString:
     def as_simple_string(self) -> str:
         return f"+{self.s}\r\n"
 
+RESP_STR = RESPString("PONG").as_simple_string()
+
+def process_connection(client_connection):
+    while True:
+        try:
+            client_connection.recv(1024):  # The server receives data from the client connection.
+            client_connection.send(RESP_STR.encode())
+        except ConnectionError:
+            break
+
+
+
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_connection, _ = server_socket.accept() # accepts a single client connection.
-    response_string = RESPString("PONG").as_simple_string()
-
-    while client_connection.recv(1024):  # While the server receives data from the client connection.
-        client_connection.send(response_string.encode())
-    server_socket.close()
+    while True:
+        client_connection, _ = server_socket.accept() # main waits for a new client connection.
+        threading.Thread(target=process_connection, args=(client_connection,)).start() # new thread is created to process the new connection, and we start it.
 
 
 if __name__ == "__main__":
